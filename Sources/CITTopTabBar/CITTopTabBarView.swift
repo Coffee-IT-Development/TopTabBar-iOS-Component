d772aea2fc37f863252dff91d805047be4589dff
Lex Brouwers <lexbrouwers@coffeeit.nl>
2022-07-11 11:43:59 +0200

    Take new example and resolve some hardcoded and illogical aspects

--- Sources/CITTopTabBar/CITTopTabBarView.swift
+++ Sources/CITTopTabBar/CITTopTabBarView.swift
@@ -17,25 +17,93 @@ public struct CITTopTabBarView: View {
         self._tabs = tabs
     }
     
+    @State var currentTab: Int = 0
+    @Namespace var namespace
+    
     public var body: some View {
-        ScrollView(.horizontal, showsIndicators: false) {
-            HStack(alignment: .center, spacing: 0) {
-                ForEach(Array(zip(tabs.indices, tabs)), id: \.0) { index, item in
-                    Button(action: { selectTab(index) }) {
-                        VStack(spacing: 0) {
-                            Text(item.title)
-                                .padding()
-                            
-                            optionalSelector(at: index)
-                                .animation(.easeOut(duration: 0.2))
-                        }
-                    }
-                }
-            }
-            .background(Color(UIColor.systemGray5))
-//            .animation(.easeOut(duration: 0.2))
+        VStack(spacing: 0) {
+            navigationBarView
+                        
+            TabView(selection: self.$currentTab, content: {
+                view1.tag(0)
+                view2.tag(1)
+                view3.tag(2)
+            })
+            .tabViewStyle(.page(indexDisplayMode: .never))
+            .edgesIgnoringSafeArea(.all)
         }
     }
+    
+    var navigationitems: [String] = ["Hello World", "This is", "A Pretty Cool Tab Bar"]
+    
+    var navigationBarView: some View {
+        ScrollView(.horizontal) {
+            HStack(alignment: .bottom, spacing: 20) {
+                ForEach(Array(zip(self.navigationitems.indices, self.navigationitems)), id: \.0, content: {
+                    index, name in
+                    navBarItem(string: name, tab: index)
+                })
+            }
+            .padding(.horizontal)
+            .edgesIgnoringSafeArea(.top)
+        }
+        .background(Color.white)
+    }
+    
+    
+    func navBarItem(string: String, tab: Int) -> some View {
+        Button {
+            self.currentTab = tab
+        } label: {
+            VStack {
+                Text(string)
+                    .font(.system(size: 13, weight: .light, design: .default))
+                    .padding(.top, 20)
+                
+                if self.currentTab == tab {
+                    Color.black.frame(height: 2)
+                        .matchedGeometryEffect(id: "underline", in: namespace, properties: .frame)
+                } else {
+                    Color.clear.frame(height: 2)
+                }
+            }
+            .animation(.spring(), value: currentTab)
+        }
+        .buttonStyle(.plain)
+    }
+    
+    var view1: some View {
+        Color.red.opacity(0.2).edgesIgnoringSafeArea(.all)
+    }
+    
+    var view2: some View {
+        Color.blue.opacity(0.2).edgesIgnoringSafeArea(.all)
+    }
+    
+    var view3: some View {
+        Color.yellow.opacity(0.2).edgesIgnoringSafeArea(.all)
+    }
+
+    
+//    public var body: some View {
+//        ScrollView(.horizontal, showsIndicators: false) {
+//            HStack(alignment: .center, spacing: 0) {
+//                ForEach(Array(zip(tabs.indices, tabs)), id: \.0) { index, item in
+//                    Button(action: { selectTab(index) }) {
+//                        VStack(spacing: 0) {
+//                            Text(item.title)
+//                                .padding()
+//
+//                            optionalSelector(at: index)
+//                                .animation(.easeOut(duration: 0.2))
+//                        }
+//                    }
+//                }
+//            }
+//            .background(Color(UIColor.systemGray5))
+////            .animation(.easeOut(duration: 0.2))
+//        }
+//    }
     
     
     @ViewBuilder
