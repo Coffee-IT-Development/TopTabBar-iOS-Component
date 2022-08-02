26e6de7ec4c0a98532b7f7a28ff3c21724ba19b5
Lex Brouwers <lexbrouwers@coffeeit.nl>
2022-08-02 17:17:03 +0200

    Re-add example app to CITTopTabBar with better name and app icon

---
+++ CITTopTabBarExample/CITTopTabBarExample/CITTopTabBarExampleApp.swift
@@ -0,0 +1,18 @@
+//
+//  CITTopTabBarExampleApp.swift
+//  CITTopTabBarExample
+//
+//  Created by Lex Brouwers on 02/08/2022.
+//  Copyright © 2022 Coffee IT. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct CITTopTabBarExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CITTopTabBarExampleView()
+        }
+    }
+}
--- CITTopTabBarExample/CITTopTabBarExample/CITTopTabBarExampleView.swift
+++ CITTopTabBarExample/CITTopTabBarExample/CITTopTabBarExampleView.swift
@@ -1,15 +1,15 @@
 //
-//  ContentView.swift
-//  CITTopTabBarRunnable
+//  CITTopTabBarExampleView.swift
+//  CITTopTabBarExample
 //
-//  Created by Lex Brouwers on 11/07/2022.
+//  Created by Lex Brouwers on 02/08/2022.
 //  Copyright © 2022 Coffee IT. All rights reserved.
 //
 
 import SwiftUI
 import CITTopTabBar
 
-struct ContentView: View {
+struct CITTopTabBarExampleView: View {
     @State var selectedTab: Int = 0
     @State var tabs: [CITTopTab] = [
         .init(title: "Long Example", badge: .circle(style: .init(color: .red))),
@@ -38,8 +38,8 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct CITTopTabBarExampleView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        CITTopTabBarExampleView()
     }
 }
